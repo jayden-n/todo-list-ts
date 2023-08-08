362ecd3dc043595a67d51201c75c50049c2d97d5
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-08-08 19:02:09 -0400

    added: added todos state & types logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-  const [inputData, setInputData] = useState('');
-
-  const todos = [{ id: 1, text: 'Finish the course' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const inputHandler = (input: string) => {
-    console.log(input);
+    setTodos([{ id: Math.random().toString(), input: input }]);
   };
 
   return (
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface TodoListProps {
-  items: { id: number; text: string }[];
+  items: { id: string; input: string }[];
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>{todo.input}</li>
       ))}
     </ul>
   );
